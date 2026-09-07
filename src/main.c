7f1a3cf6c3a27818dc3d2c309086d4f609ad9bff
kyleuphus <kyleuphus@gmail.com>
2026-09-07 17:36:20 -0400

    validate port argument

--- src/main.c
+++ src/main.c
@@ -1,4 +1,7 @@
 #include "server.h"
+#include "types.h"
+
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -9,6 +12,14 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    int port = atoi(argv[1]);
-    return run_server(port);
+    char* end;
+    errno = 0;
+    long port = strtol(argv[1], &end, 10);
+    if (end == argv[1] || *end != '\0' || errno == ERANGE || port < 1 ||
+        port > 65535) {
+        fprintf(stderr, "port must be 1-65535\n");
+        return 1;
+    }
+
+    return run_server((u16)port);
 }
