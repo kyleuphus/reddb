9e60681c7db31cd34477c7f9376ac637856a0e55
kyleuphus <kyleuphus@gmail.com>
2026-09-09 15:14:59 -0400

    add command dispatch for SET, GET, DEL, EXISTS, and INCR

--- include/command.h
+++ include/command.h
@@ -1,7 +1,8 @@
 #ifndef COMMAND_H
 #define COMMAND_H
 #include "buffer.h"
+#include "hashtable.h"
 #include "types.h"
 
-void dispatch(char** argv, usize* arglen, i32 argc, buffer_t* out);
+void dispatch(char** argv, usize* arglen, i32 argc, buffer_t* out, ht* t);
 #endif
--- src/command.c
+++ src/command.c
@@ -1,10 +1,15 @@
 #include "command.h"
 #include "resp.h"
 
+#include <errno.h>
+#include <inttypes.h>
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <strings.h>
 
-void dispatch(char** argv, usize* arglen, i32 argc, buffer_t* out) {
+void dispatch(char** argv, usize* arglen, i32 argc, buffer_t* out, ht* t) {
 
     if (argc == 0) {
         return;
@@ -26,6 +31,103 @@ void dispatch(char** argv, usize* arglen, i32 argc, buffer_t* out) {
             resp_write_error(
                 out, "ERR wrong number of arguments for 'echo' command");
         }
+    } else if (strcasecmp(argv[0], "SET") == 0) {
+        if (argc == 3) {
+            if (ht_set(t, argv[1], arglen[1], argv[2], arglen[2])) {
+                resp_write_simple(out, "OK");
+            } else {
+                resp_write_error(out, "ERR out of memory");
+            }
+        } else {
+            resp_write_error(out,
+                             "ERR wrong number of arguments for 'set' command");
+        }
+    } else if (strcasecmp(argv[0], "GET") == 0) {
+        usize outlen;
+        if (argc == 2) {
+            const char* value = ht_get(t, argv[1], arglen[1], &outlen);
+            if (value != NULL) {
+                resp_write_bulk(out, value, outlen);
+            } else {
+                resp_write_null(out);
+            }
+        } else {
+            resp_write_error(out,
+                             "ERR wrong number of arguments for 'get' command");
+        }
+    } else if (strcasecmp(argv[0], "DEL") == 0) {
+        if (argc == 2) {
+            if (ht_delete(t, argv[1], arglen[1])) {
+                resp_write_integer(out, 1);
+            } else {
+                resp_write_integer(out, 0);
+            }
+        } else {
+            resp_write_error(out,
+                             "ERR wrong number of arguments for 'del' command");
+        }
+    } else if (strcasecmp(argv[0], "EXISTS") == 0) {
+        usize outlen;
+        if (argc == 2) {
+            if (ht_get(t, argv[1], arglen[1], &outlen) != NULL) {
+                resp_write_integer(out, 1);
+            } else {
+                resp_write_integer(out, 0);
+            }
+        } else {
+            resp_write_error(
+                out, "ERR wrong number of arguments for 'exists' command");
+        }
+
+    } else if (strcasecmp(argv[0], "INCR") == 0) {
+        usize outlen;
+        if (argc == 2) {
+            const char* value = ht_get(t, argv[1], arglen[1], &outlen);
+            char tmp[32];
+
+            if (value == NULL) {
+                if (ht_set(t, argv[1], arglen[1], "1", 1)) {
+                    resp_write_integer(out, 1);
+                } else {
+                    resp_write_error(out, "ERR out of memory");
+                }
+            } else if (outlen >= sizeof(tmp)) {
+                resp_write_error(out,
+                                 "ERR value is not an integer or out of range");
+            } else {
+
+                memcpy(tmp, value, outlen);
+                tmp[outlen] = '\0';
+
+                char* endptr;
+                errno = 0;
+                i64 ivalue = strtoll(tmp, &endptr, 10);
+
+                if (endptr == tmp || endptr != tmp + outlen ||
+                    errno == ERANGE) {
+                    resp_write_error(
+                        out, "ERR value is not an integer or out of range");
+                } else if (ivalue >= LLONG_MAX)
+                    resp_write_error(
+                        out, "ERR value is not an integer or out of range");
+                else {
+                    ivalue++;
+                    char int_str[32];
+                    snprintf(int_str, sizeof(int_str), "%" PRId64, ivalue);
+                    if (ht_set(t, argv[1], arglen[1], int_str,
+                               strlen(int_str))) {
+                        resp_write_integer(out, ivalue);
+                    } else {
+                        resp_write_error(out, "ERR out of memory");
+                    }
+                }
+            }
+
+        } else {
+            resp_write_error(
+                out, "ERR wrong number of arguments for 'incr' command");
+        }
+
     } else {
         resp_write_error(out, "ERR unknown command");
     }
