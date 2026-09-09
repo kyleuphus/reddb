bc962181a19e0bc836bcea5f8d4beca956d6974c
kyleuphus <kyleuphus@gmail.com>
2026-09-09 16:30:25 -0400

    tighten integer parsing in INCR

--- src/command.c
+++ src/command.c
@@ -101,16 +101,23 @@ void dispatch(char** argv, usize* arglen, i32 argc, buffer_t* out, ht* t) {
 
                 char* endptr;
                 errno = 0;
+
                 i64 ivalue = strtoll(tmp, &endptr, 10);
+                usize i = (tmp[0] == '-') ? 1 : 0;
+                b8 leading_ok = (tmp[i] >= '1' && tmp[i] <= '9');
+                b8 is_zero = (outlen == 1 && tmp[0] == '0');
 
                 if (endptr == tmp || endptr != tmp + outlen ||
                     errno == ERANGE) {
                     resp_write_error(
                         out, "ERR value is not an integer or out of range");
-                } else if (ivalue >= LLONG_MAX)
+                } else if (ivalue >= LLONG_MAX) {
                     resp_write_error(
                         out, "ERR value is not an integer or out of range");
-                else {
+                } else if (outlen <= i || !(leading_ok || is_zero)) {
+                    resp_write_error(
+                        out, "ERR value is not an integer or out of range");
+                } else {
                     ivalue++;
                     char int_str[32];
                     snprintf(int_str, sizeof(int_str), "%" PRId64, ivalue);
@@ -122,12 +129,10 @@ void dispatch(char** argv, usize* arglen, i32 argc, buffer_t* out, ht* t) {
                     }
                 }
             }
-
         } else {
             resp_write_error(
                 out, "ERR wrong number of arguments for 'incr' command");
         }
-
     } else {
         resp_write_error(out, "ERR unknown command");
     }
